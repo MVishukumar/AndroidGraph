a331aef09556802482f248d1579d2ae56af4d17f
vishukumar <beingvishu@gmail.com>
2018-04-04 14:47:25 +0530

    Added touch listener.

--- app/src/main/java/vishukumar/com/graph/MainActivity.java
+++ app/src/main/java/vishukumar/com/graph/MainActivity.java
@@ -1,7 +1,11 @@
 package vishukumar.com.graph;
 
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -11,21 +15,30 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener {
 
     BarChart barChart;
+    protected RectF mOnValueSelectedRectF = new RectF();
+    ArrayList<BarEntry> barEntries = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("tag", "app started");
+
         barChart = (BarChart) findViewById(R.id.id_bargraph);
+        barChart.setOnChartValueSelectedListener(this);
         barChart.setDrawBarShadow(false);
         barChart.setDrawValueAboveBar(true);
         barChart.setMaxVisibleValueCount(100);
@@ -34,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         barChart.getDescription().setEnabled(false);
 
 
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
+
         barEntries.add(new BarEntry(0, 22.0f));
         barEntries.add(new BarEntry(1, 42.0f));
         barEntries.add(new BarEntry(2, 12.0f));
@@ -78,6 +91,20 @@ public class MainActivity extends AppCompatActivity {
         l.setXEntrySpace(4f);
 
     }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Log.d("tag", "Value selected");
+
+        Toast.makeText(this,barEntries.indexOf(e)+"",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.d("tag", "Nothing selected");
+    }
+
 }
 
 
